f6821568f466c119bf9920566772bc0d83127fc2
Sam Bao <sam.b5687@gmail.com>
2016-11-18 15:47:54 -0700

    initial upload

---
+++ Webtest/index.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Webtest.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <input type="datetime-local" name ="metric1"/>
+        <input type="datetime-local" name ="metric2"/>
+         Metric #1:
+        <input list="metricname" id="M1"/>           
+        <datalist id="metricname">
+            <asp:Literal ID="metric1" runat="server"></asp:Literal>
+        </datalist>
+        Metric #2:
+        <input list="metricnames" id="M2"/>
+        <datalist id="metricnames">
+           <asp:Literal ID="metric2" runat="server"></asp:Literal>
+        </datalist>
+    </div>
+         <asp:Button id="Submit"
+           Text="Submit"
+           OnClick="submit_Click" 
+           runat="server"/>
+
+    </form>
+</body>
+</html>
---
+++ Webtest/index.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace Webtest
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //string url = "162.246.157.107/metrics/index.json";
+            Uri u = new Uri("http://162.246.157.107/metrics/index.json",UriKind.Absolute);
+            var request = WebRequest.Create(u);
+            request.ContentType = "application/json; charset=utf-8";
+            string text;
+            var response = (HttpWebResponse)request.GetResponse();
+
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                text = sr.ReadToEnd();
+                text = text.Replace("[", "");
+                text = text.Replace("]", "");
+                text = text.Replace("\"", "");
+                string[] t = text.Split(',');
+                //Literal myLitCtrl = (Literal)FindControl("litDescription");
+                string results = "";
+                foreach(var i in t)
+                {
+                    results += "<option value =\""+i+"\"></option>";
+                }
+                metric1.Text = results;
+                metric2.Text = results;
+
+                
+            }
+        }
+        protected virtual void submit_click(    EventArgs e)
+        {
+            string metricname1 = 
+        }
+
+
+    }
+}
